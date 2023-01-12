a6cad26e88a2b2568576541128ec9e0262922f31
mamiyqa <mamiy.a.a@mail.ru>
2023-01-12 17:12:01 +0300

    начало кринжа

--- main.py
+++ main.py
@@ -1,16 +1,40 @@
-# This is a sample Python script.
+import os
+import discord
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+from discord.ext import commands
+
+intents = discord.Intents.all()
+
+settings = {
+    'token': os.getenv('BOT_TOKEN'),
+    'bot': 'MaMaY_BOT',
+    'id': os.getenv('BOT_ID'),
+    'prefix': '$'
+}
+
+bot = commands.Bot(command_prefix=settings['prefix'], intents=intents)
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+@bot.command('rand')  # Не передаём аргумент pass_context, так как он был нужен в старых версиях.
+async def hello(ctx):  # Создаём функцию и передаём аргумент ctx.
+    author = ctx.message.author  # Объявляем переменную author и записываем туда информацию об авторе.
+
+    await ctx.send(
+        f'Hello, {author.mention}!')  # Выводим сообщение с упоминанием автора, обращаясь к переменной author.@bot.command('rand')  # Не передаём аргумент pass_context, так как он был нужен в старых версиях.
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+@bot.command(commands=['пидор','леха пидор'])  # Не передаём аргумент pass_context, так как он был нужен в старых версиях.
+async def test(ctx):  # Создаём функцию и передаём аргумент ctx.
+    author = ctx.message.author  # Объявляем переменную author и записываем туда информацию об авторе.
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    await ctx.send(
+        f'сам ты пидор, {author.mention}!')  # Выводим сообщение с упоминанием автора, обращаясь к переменной author.
+
+@bot.command(commands='*')
+async def tes(ctx):
+    author = ctx.message.author  # Объявляем переменную author и записываем туда информацию об авторе.
+
+    await ctx.send(
+        f'{author.mention}, сам ты {ctx.message}!')
+
+bot.run(settings['token'])  # Обращаемся к словарю settings с ключом token, для получения токена
